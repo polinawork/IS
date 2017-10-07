61e0870495741f13ef9928115722a0240bb6fb5e
polinawork <dmitrievnapolina@mail.ru>
2017-10-07 20:55:28 +0300

    Task 4

--- task4_scannnerIntoStream/Main.java
+++ task4_scannnerIntoStream/Main.java
@@ -4,20 +4,25 @@ import java.util.stream.*;
 
 public class Main {
 	public static void main(String[] args){
-		Scanner scanner = new Scanner(System.in);
-		if(scanner.hasNextInt()){
+        //String type = "int";	
+        //String type = "double";
+        //String type = "line";
+        String type = "word";
+        System.out.println("Input values: ");
+        Scanner scanner = new Scanner(System.in);     
+        if(type=="int"){
 			IntStream stream = ScannerIntoStream.intoIntStream(scanner);
 			stream.forEach(System.out::println);
 		}
-		else if(scanner.hasNextDouble()){
+		else if(type=="double"){
 			DoubleStream stream = ScannerIntoStream.intoDoubleStream(scanner);
 			stream.forEach(System.out::println);	
 		}
-		else if(scanner.hasNextLine()){
+		else if(type=="line"){
 			Stream<String> stream = ScannerIntoStream.intoLinesStream(scanner);
 			stream.forEach(System.out::println);
 		}
-		else if(scanner.hasNext()){
+		else if(type=="word"){
 			Stream<String> stream = ScannerIntoStream.intoWordStream(scanner);
 			stream.forEach(System.out::println);
 		}
